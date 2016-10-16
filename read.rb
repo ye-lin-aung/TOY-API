6fcfc111ddec6cf920f8755eeb512b7da706ed5e
Ye lin aung <hello.yelinaung@gmail.com>
2016-10-16 13:05:20 +0630

    Change Regex with Nokogiri for performance

--- read.rb
+++ read.rb
@@ -1,29 +1,31 @@
 require 'open-uri'
 require 'json'
 require 'sinatra'
-
+require 'Nokogiri'
 
 get '/' do
+	
+	content_type :json, 'charset' => 'utf-8'
+	Struct.new("Toy", :id, :regon_number,:offence_date,:offence_time,:offence_place,:offence_type)
+# REGEX
+# @regular_exp = '<tr><td class="idnumber">(.*)<\/td><td class="regonumber">(.*)<\/td><td class="offencedate">(.*)<\/td><td class="offencetime">(.*)<\/td><td class="offenceplace">(.*)<\/td><td class="offencetype">(.*)<\/td><\/tr>'
 
-content_type :json, 'charset' => 'utf-8'
-@regular_exp = '<tr><td class="idnumber">(.*)<\/td><td class="regonumber">(.*)<\/td><td class="offencedate">(.*)<\/td><td class="offencetime">(.*)<\/td><td class="offenceplace">(.*)<\/td><td class="offencetype">(.*)<\/td><\/tr>'
-Struct.new("Toy", :id, :regon_number,:offence_date,:offence_time,:offence_place,:offence_type)
 toys = Array.new
-file_contents = open("http://winlwinmoe.com/toy/"){
-	|f| 
-	lines = f.readlines
-	lines.each do |line|
-		match =  line.match(@regular_exp)	
-		if(match != nil)then
-			
-			toy = Struct::Toy.new(match[1].force_encoding("utf-8"),match[2].force_encoding("utf-8"),match[3].force_encoding("utf-8"),match[4].force_encoding("utf-8"),match[5].force_encoding("utf-8"),match[6].force_encoding("utf-8"))
+file_contents = Nokogiri::HTML(open("http://winlwinmoe.com/toy/"))
 
-			toys << toy	
-			
-		end	 
-		
+
+count = 0
+file_contents.css('tr').each { 
+	|data| 
+	if(count>1) then
+		toy = Struct::Toy.new(data.css(".idnumber").text.force_encoding("utf-8"),data.css(".regonumber").text.force_encoding("utf-8"),data.css(".offencedate").text.force_encoding("utf-8"),data.css(".offencetime").text.force_encoding("utf-8"),data.css(".offenceplace").text.force_encoding("utf-8"),data.css(".offencetype").text.force_encoding("utf-8"))
+		toys << toy	
+		puts "_______________>"
+		puts count
 	end
+	count = count + 1	
 }
 toys.map { |o| Hash[o.each_pair.to_a] }.to_json
+
 
 end
